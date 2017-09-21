a582b50a7542adb83dcbfad281ab877937a07d37
Ben Chan <benchanviolin@gmail.com>
2017-09-21 05:56:36 -0400

    type: fix | Move MyStatusBar logic.

--- App.js
+++ App.js
@@ -2,13 +2,24 @@ import React from 'react';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
 import reducer from './src/reducer';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, StatusBar } from 'react-native';
+import { primaryBackgroundColor } from './src/themes/default/colors';
+import { Constants } from 'expo';
+
+function MyStatusBar ({backgroundColor, ...props}) {
+  return (
+    <View style={{ backgroundColor, height: Constants.statusBarHeight }}>
+      <StatusBar translucent backgroundColor={backgroundColor} {...props} />
+    </View>
+  )
+}
 
 export default class App extends React.Component {
   render() {
     return (
       <Provider store={createStore(reducer)}>
         <View style={styles.container}>
+          <MyStatusBar backgroundColor={primaryBackgroundColor} />
           <Text>HELLO WORLD</Text>
         </View>
       </Provider>
--- src/components/statusBar/statusBar.js
+++
@@ -1,7 +0,0 @@
-export function statusBar ({backgroundColor, ...props}) {
-  return (
-    <View style={{ backgroundColor, height: Constants.statusBarHeight }}>
-      <StatusBar translucent backgroundColor={backgroundColor} {...props} />
-    </View>
-  )
-}

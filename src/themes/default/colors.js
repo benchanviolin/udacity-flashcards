c86f3cfa17e1f3935677085aa3240becac64f585
Ben Chan <benchanviolin@gmail.com>
2017-09-21 05:32:32 -0400

    type: feat | Add theme colors.

---
+++ src/themes/default/colors.js
@@ -0,0 +1,13 @@
+/*
+https://envato.com/blog/8-color-scheme-trends-mobile-app-design/
+5. Pastel, muted colors
+*/
+
+export const primaryBackgroundColor = '#7babed';
+export const primaryColor = '#fff';
+export const dangerBackgroundColor = '#f6c2d8';
+export const dangerColor = '#e59cb0';
+export const highlightBackgroundColor = '#fcebbf';
+export const highlightColor = '#e7be52';
+export const textColor = '#000';
+export const textDataColor = '#ced6e3';

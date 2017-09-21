bbf2a1c33e9fd22d97b00b5555f77a2b85860f6b
Ben Chan <benchanviolin@gmail.com>
2017-09-21 05:15:32 -0400

    type: feat | Create statusBar component.

---
+++ src/components/statusBar/statusBar.js
@@ -0,0 +1,7 @@
+export function statusBar ({backgroundColor, ...props}) {
+  return (
+    <View style={{ backgroundColor, height: Constants.statusBarHeight }}>
+      <StatusBar translucent backgroundColor={backgroundColor} {...props} />
+    </View>
+  )
+}

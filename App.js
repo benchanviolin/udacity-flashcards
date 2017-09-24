5a7e93f540a8ae474feae79be8c28326d501656d
Ben Chan <benchanviolin@gmail.com>
2017-09-23 20:13:16 -0400

    git: chore | Add middleware logging.

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import { Provider } from 'react-redux';
 import combinedReducer from './src/store/combinedReducer';
 import { StyleSheet, Text, View, StatusBar } from 'react-native';
@@ -8,16 +8,34 @@ import MyStatusBar from './src/navigation/myStatusBar';
 import MyStore from './src/store/myStore';
 import { MyNavigator } from './src/navigation/myNavigator';
 
+const logger = store => next => action => {
+  console.group(action.type);
+  console.info('dispatching', action);
+  let result = next(action);
+  console.log('next state', store.getState());
+  console.groupEnd(action.type);
+  return result;
+}
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
+
+const store = createStore(
+  combinedReducer,
+  composeEnhancers(
+    applyMiddleware(logger)
+  )
+)
+
 export default function App ({}) {
   return (
-    <Provider store={createStore(combinedReducer)}>
+    <Provider store={store}>
       <View style={styles.container}>
         <MyStore />
         <MyStatusBar backgroundColor={primaryBackgroundColor} />
         <MyNavigator />
       </View>
     </Provider>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
